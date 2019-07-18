886736270a39ccf96e9932d64424d18ffc3e8a69
梅宮 <euthanasia045@gmail.com>
2019-07-18 12:54:18 +0900

    prac Scanner

---
+++ janken/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	fmt.Println(sc)
+}
+
+func lineParse() string {
+	return "a"
+}
+
+func janken(cpu_hand, you_hand int) string {
+	return "test"
+}
