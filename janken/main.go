fefa291c7bd23fd710c9c061af0d7f81769be53e
梅宮 <euthanasia045@gmail.com>
2019-07-18 18:37:16 +0900

    scanの練習 バグあり

--- janken/main.go
+++ janken/main.go
@@ -8,11 +8,20 @@ import (
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
-	fmt.Println(sc)
+	txt := lineParse(sc)
+	fmt.Println(txt)
 }
 
-func lineParse() string {
-	return "a"
+func lineParse(sc bufio.Scanner) []string {
+	line := []string
+	read_judge := sc.Scan()
+	if read_judge {
+		txt := sc.Text()
+		for _, s := range txt{
+			line = append(line, s)
+		}
+	}
+	return txt
 }
 
 func janken(cpu_hand, you_hand int) string {
