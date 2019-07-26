c6bc8ea667cb044099aa0ab5a91941d6befc68fd
梅宮 <euthanasia045@gmail.com>
2019-07-26 18:30:22 +0900

    add deque standard input process

--- aoj/deque.go
+++ aoj/deque.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"os"
 	"bufio"
-	//"strconv"
+	"strconv"
+	"strings"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
@@ -14,12 +15,25 @@ func nLine() string {
 	return sc.Text()
 }
 
+func strToIntArray(s string) []int {
+	str_arr := strings.Split(s, " ")
+	int_arr := []int{}
+	for _, val := range str_arr {
+		int_val, err := strconv.Atoi(val)
+		if err == nil {
+			int_arr = append(int_arr, int_val)
+		}
+	}
+	return int_arr
+}
+
 func main() {
 	var q_num int
 	fmt.Scan(&q_num)
-	order := []string{}
+	order := [][]int{}
 	for range(make([]int, q_num)) {
-		order = append(order, nLine())
+		one_order := nLine()
+		order = append(order, strToIntArray(one_order))
 	}
-
+	fmt.Println(order)
 }
