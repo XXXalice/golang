4d0e49f182942e21c1d80b724b8d365704745816
梅宮 <euthanasia045@gmail.com>
2019-10-23 16:56:29 +0900

    add shuffle function

--- blackjack/main.go
+++ blackjack/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func BuiltDeck() []string {
@@ -15,6 +16,14 @@ func BuiltDeck() []string {
 		}
 	}
 	return deck
+}
+
+func Shuffle(d []string) []string {
+	n := len(d)
+	for i := n - 1; i >= 0; i-- {
+		j := rand.Intn(i + 1)
+		d[i], d[j] = d[j], d[i]
+	}
 }
 
 func main() {
