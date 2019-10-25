745daa6f1b34253199622a37c5337bdf70f1fb14
梅宮 <euthanasia045@gmail.com>
2019-10-25 17:31:54 +0900

    ライブラリの分割

---
+++ blackjack/card/card.go
@@ -0,0 +1,30 @@
+package card
+
+import "math/rand"
+
+func BuiltDeck() []string {
+	number := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+	mark := []string{"spade","heart","clover","diamond"}
+	deck := make([]string, 0)
+	for _, n := range number {
+		for _, m := range mark {
+			card := m + "_" + n
+			deck = append(deck, card)
+		}
+	}
+	return deck
+}
+
+func Shuffle(d []string) {
+	n := len(d)
+	for i := n - 1; i >= 0; i-- {
+		j := rand.Intn(i + 1)
+		d[i], d[j] = d[j], d[i]
+	}
+}
+
+func Draw(d []string) (string, []string) {
+	drawCard := d[0]
+	d = append(d[:0], d[1:]...)
+	return drawCard, d
+}
