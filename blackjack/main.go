c8f7a4a39371c9bd4c584b6f1dd7e92dce9afcb6
梅宮 <euthanasia045@gmail.com>
2019-10-28 15:45:03 +0900

    CardJudge関数の実装

--- blackjack/main.go
+++ blackjack/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"./card"
+	"strings"
+	"strconv"
 )
 
-type Dealer struct {
+type BlackjackDealer struct {
 	Money int
 }
 
-func (d Dealer) CardJudge(p_card string, d_card string) {
-
+func (d Dealer) CardJudge(p_card string, d_card string) bool {
+	p_num, _ := strconv.Atoi(strings.Split(p_card, "_")[1])
+	d_num, _ := strconv.Atoi(strings.Split(d_card, "_")[1])
+	var res bool
+	if p_num > d_num {
+		res = true
+	} else {
+		res = false
+	}
+	return res
 }
 
 //func (d Dealer) Split(str string, idf string) []string {
